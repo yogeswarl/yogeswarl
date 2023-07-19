5af74421dc702d96ca7669e1a6e7150d02bd90a1
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2023-07-18 22:50:36 -0400

    setup and sass testing with Minimal integration.

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+
+const inter = Poppins({weight:["100","200"] ,subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Yogeswar Lakshmi Narayanan | Full Stack Developer ',
+  description: 'Welcome to My Portfolio Website 🎉',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
