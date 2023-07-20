c89649b8eb35c6c13431de81dd5f64057d892c4d
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2023-07-20 08:14:20 -0400

    intersection observer working and stars anim fix

--- src/components/Home/section.tsx
+++ src/components/Home/section.tsx
@@ -1,14 +1,29 @@
-
+"use client";
 import * as React from "react";
+import {useIntersectionObserver} from "@/helper/use-hooks";
 import { TagClassnames } from "@/types/interface";
+import Style from '../../app/page.module.scss'
 interface Props extends TagClassnames {
   children: React.ReactNode
 }
+
 export const MainSection : React.FC<Props> = ({
   classname,
   children,
 }): JSX.Element => {
-  return <section className={`${Array.isArray(classname) ? classname.join(" "): classname}`}>{children}</section>;
+  const ref = React.useRef<HTMLElement | null>(null)
+  const entry = useIntersectionObserver(ref, {})
+  const isVisible = !!entry?.isIntersecting
+  console.log(`Render classes ${children}`, { isVisible })
+  if(isVisible){
+    console.log("Visible")
+    ref.current?.classList.add(`${Style["slide-in"]}`)
+  }
+  else {
+    ref.current?.classList.remove(`${Style["slide-in"]}`)
+  }
+
+  return <section ref = {ref} className={`${Array.isArray(classname) ? classname.join(" "): classname}`}>{children}</section>;
 }
 
         
