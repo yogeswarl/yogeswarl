ae25d0488edcbe077b9bc23fd6a27cbe6eae54bc
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2023-07-20 00:10:46 -0400

    screen 1 done. Animate on Scroll left

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 
-const inter = Poppins({weight:["300","700"] ,subsets: ['latin'] })
+const inter = Poppins({weight:["300","400","600"] ,subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Yogeswar Lakshmi Narayanan | Full Stack Developer ',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,21 +7,28 @@ export default function Home() {
     <Fragment>
       {/* need to become a component */}
       <header></header>
-      <main className={`${[Style.parent,Style["pos-rel"], Style.main].join(" ")}`}>
-        <Stars classname={[Style["pos-abs"], Style["top-right"], Style.star]}/>
-        <Stars classname={[Style["pos-abs"], Style["top-left"], Style.star]}/>
-        <Stars classname={[Style["pos-abs"], Style["bottom-right"], Style.star]}/>
-        <Stars classname={[Style["pos-abs"], Style["bottom-left"], Style.star]}/>
+      <Stars classname={[Style["pos-fixed"], Style["top-right"], Style.star]}/>
+      <Stars classname={[Style["pos-fixed"], Style["top-left"], Style.star]}/>
+      <Stars classname={[Style["pos-fixed"], Style["center"], Style.star]}/>
+      <Stars classname={[Style["pos-fixed"], Style["bottom-right"], Style.star]}/>
+      <Stars classname={[Style["pos-fixed"], Style["bottom-left"], Style.star]}/>
+      <main className={`${Style.main} ${Style.parent}`}>
         
         {/* its a component, need to make it with a pass of children */}
-        <MainSection classname={[Style.fx, Style["fx-cv"], Style["fx-ch"], Style["main-section"]]}>
-          <h1>Home</h1>
+        <MainSection classname={[Style.fx, Style.container, Style["fx-cv"], Style["fx-ch"], Style["main-section"],Style["slide-in"]]}>
+          <div className={`${Style['inner-container']}`}>
+            <h1 className={`${Style.heading} ${Style["fw-semibold"]}`}>Hey 👋 , I am Yogeswar. Welcome to my portfolio</h1>
+          </div>
         </MainSection>
-        <MainSection classname={[Style.fx, Style["fx-cv"], Style["fx-ch"], Style["main-section"]]}>
-          <h1>Home</h1>
+        <MainSection classname={[Style.fx, Style.container, Style["fx-cv"], Style["fx-ch"], Style["main-section"]]}>
+          <div className={`${Style['inner-container']}`}>
+            <h1 className={`${Style.heading} ${Style["fw-semibold"]}`}>Hey 👋 , I am Yogeswar. Welcome to my portfolio 2</h1>
+          </div>
         </MainSection>
-        <MainSection classname={[Style.fx, Style["fx-cv"], Style["fx-ch"], Style["main-section"]]}>
-          <h1>Home</h1>
+        <MainSection classname={[Style.fx, Style.container, Style["fx-cv"], Style["fx-ch"], Style["main-section"]]}>
+          <div className={`${Style['inner-container']}`}>
+            <h1 className={`${Style.heading} ${Style["fw-semibold"]}`}>Hey 👋 , I am Yogeswar. Welcome to my portfolio 3</h1>
+          </div>
         </MainSection>
 
       </main>
