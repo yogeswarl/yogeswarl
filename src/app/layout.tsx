461ef125b22b5605b5ff780db4ed12a03a547cb5
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2023-07-22 22:48:16 -0400

    add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { poppins } from './font'
 export const metadata: Metadata = {
-  title: 'Yogeswar Lakshmi Narayanan | Full Stack Developer ',
+  title: 'Yogeswar Lakshmi Narayanan | Full Stack Developer | Portfolio Website',
   description: 'Welcome to My Portfolio Website 🎉',
 }
 
@@ -13,6 +13,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="16x16" type="image/ico" /> 
+        <link rel="icon" href="favicon-16x16.png" sizes="16x16" type="image/png" />
+        <link rel="icon" href="favicon-32x32.png" sizes="32x32" type="image/png" />
+        <link rel='apple-touch-icon' href='/apple-touch-icon.png' type="image/png"/>
+      </head>
       <body className={`${poppins.className}`}>{children}</body>
     </html>
   )
